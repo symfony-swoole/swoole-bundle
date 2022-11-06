e8a0f3121bc2c446f0472a2b18ff78228a7fbca1
Rastusik <mfris@pixelfederation.com>
2022-11-06 16:20:00 +0100

    refactor(coroutines): service pools unification, servic pool locking unification

--- src/Bridge/Symfony/Container/ServicePool/UnmanagedFactoryServicePool.php
+++ src/Bridge/Symfony/Container/ServicePool/UnmanagedFactoryServicePool.php
@@ -4,58 +4,26 @@ declare(strict_types=1);
 
 namespace K911\Swoole\Bridge\Symfony\Container\ServicePool;
 
-final class UnmanagedFactoryServicePool implements ServicePool
+use K911\Swoole\Component\Locking\Locking;
+
+final class UnmanagedFactoryServicePool extends BaseServicePool
 {
     /**
      * @var \Closure(): object
      */
     private \Closure $instantiator;
 
-    /**
-     * @var array<int, object>
-     */
-    private array $freePool = [];
-
-    /**
-     * @var array<int, object>
-     */
-    private array $assignedPool = [];
-
-    public function __construct(\Closure $instantiator)
+    public function __construct(\Closure $instantiator, string $lockingKey, Locking $locking, int $instancesLimit = 50)
     {
         $this->instantiator = $instantiator;
+
+        parent::__construct($lockingKey, $locking, $instancesLimit);
     }
 
-    public function get(): object
+    protected function newServiceInstance(): object
     {
-        $cId = $this->getCoroutineId();
-
-        if (isset($this->assignedPool[$cId])) {
-            return $this->assignedPool[$cId];
-        }
-
-        if (!empty($this->freePool)) {
-            return $this->assignedPool[$cId] = array_shift($this->freePool);
-        }
-
         $instantiator = $this->instantiator;
 
-        return $this->assignedPool[$cId] = $instantiator();
-    }
-
-    public function releaseForCoroutine(int $cId): void
-    {
-        if (!isset($this->assignedPool[$cId])) {
-            return;
-        }
-
-        $service = $this->assignedPool[$cId];
-        unset($this->assignedPool[$cId]);
-        $this->freePool[] = $service;
-    }
-
-    private function getCoroutineId(): int
-    {
-        return \Co::getCid();
+        return $instantiator();
     }
 }
