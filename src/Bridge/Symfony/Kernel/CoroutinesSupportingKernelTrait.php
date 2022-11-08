bbafcf378859906f684e9a3a729516755bc94c3c
Rastusik <mfris@pixelfederation.com>
2022-11-08 22:01:31 +0100

    refactor(kerneltrait): renamed CoroutineSupportingKernel to CoroutineSupportingKernelTrait + docs regarding usage

--- src/Bridge/Symfony/Kernel/CoroutinesSupportingKernelTrait.php
+++ src/Bridge/Symfony/Kernel/CoroutinesSupportingKernelTrait.php
@@ -7,7 +7,7 @@ namespace K911\Swoole\Bridge\Symfony\Kernel;
 use K911\Swoole\Bridge\Symfony\Container\BlockingContainer;
 use K911\Swoole\Reflection\FinalClassModifier;
 
-trait CoroutinesSupportingKernel
+trait CoroutinesSupportingKernelTrait
 {
     /**
      * for the coroutines to work properly, the kernel __clone method has to be overriden,
@@ -17,11 +17,20 @@ trait CoroutinesSupportingKernel
     {
     }
 
+    /**
+     * this overrides the container class to a container, which is able to block the first instatiation
+     * of requested service instance (because class autoloading is IO operation, which switches coroutine context).
+     * the blocking ensures that only one service instance will be created concurrently and it will be registered
+     * correctly in the container.
+     */
     protected function getContainerBaseClass(): string
     {
         return BlockingContainer::class;
     }
 
+    /**
+     * this initializes logic which removes the final flag from proxified classes (if they are final)
+     */
     protected function initializeContainer()
     {
         FinalClassModifier::initialize($this->getCacheDir());
