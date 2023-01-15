1e88a2f30c6a9f37f73bbd238cbf990757651205
Rastusik <mfris@pixelfederation.com>
2023-01-15 17:44:35 +0100

    feat(testing): tests are able to run also in prod env with container infline factories enabled, coroutines tests were changed accordingly

--- tests/Feature/SwooleServerCoroutinesTest.php
+++ tests/Feature/SwooleServerCoroutinesTest.php
@@ -15,13 +15,15 @@ final class SwooleServerCoroutinesTest extends ServerTestCase
     protected function setUp(): void
     {
         $this->markTestSkippedIfXdebugEnabled();
+        $this->deleteVarDirectory();
     }
 
-    public function testCoroutinesWithDebugOn(): void
+    /**
+     * @dataProvider coroutineTestDataProvider
+     */
+    public function testCoroutinesWithEnvs(array $envs): void
     {
-        $clearCache = $this->createConsoleProcess([
-            'cache:clear',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        $clearCache = $this->createConsoleProcess(['cache:clear'], $envs);
         $clearCache->setTimeout(5);
         $clearCache->disableOutput();
         $clearCache->run();
@@ -32,7 +34,7 @@ final class SwooleServerCoroutinesTest extends ServerTestCase
             'swoole:server:start',
             '--host=localhost',
             '--port=9999',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        ], $envs);
 
         $serverStart->setTimeout(5);
         $serverStart->disableOutput();
@@ -92,118 +94,53 @@ final class SwooleServerCoroutinesTest extends ServerTestCase
         });
     }
 
-    public function testCoroutinesWithDebugOff(): void
+    /**
+     * @dataProvider coroutineTestDataProvider
+     */
+    public function testCoroutinesAndDoctrineWithEnvs(array $envs): void
     {
-        $clearCache = $this->createConsoleProcess([
-            'cache:clear',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        $clearCache = $this->createConsoleProcess(['cache:clear'], $envs);
         $clearCache->setTimeout(5);
         $clearCache->disableOutput();
         $clearCache->run();
 
         $this->assertProcessSucceeded($clearCache);
 
-        $serverStart = $this->createConsoleProcess([
-            'swoole:server:start',
-            '--host=localhost',
-            '--port=9999',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
-
-        $serverStart->setTimeout(5);
-        $serverStart->disableOutput();
-        $serverStart->run();
-
-        $this->assertProcessSucceeded($serverStart);
-
-        $this->runAsCoroutineAndWait(function (): void {
-            $this->deferServerStop();
-
-            $initClient = HttpClient::fromDomain('localhost', 9999, false);
-            $this->assertTrue($initClient->connect());
-
-            $start = microtime(true);
-            $wg = new WaitGroup();
-
-            for ($i = 0; $i < 10; ++$i) {
-                go(function () use ($wg): void {
-                    $wg->add();
-                    $client = HttpClient::fromDomain('localhost', 9999, false);
-                    $this->assertTrue($client->connect());
-                    $response = $client->send('/sleep')['response']; // request sleeps for 2 seconds
-                    $this->assertSame(200, $response['statusCode']);
-                    $this->assertStringContainsString('text/html', $response['headers']['content-type']);
-                    // this also tests custom compile processor, which proxifies the sleeper service,
-                    // so multiple instances will be created (one for each coroutine), so the counter in them
-                    // is 1 for the first x concurrent requests
-                    $this->assertMatchesRegularExpression('/Sleep was fine\. Count was [1-9]\./', $response['body']);
-                    $this->assertStringContainsString('Service was proxified.', $response['body']);
-                    $this->assertStringContainsString('Service2 was proxified.', $response['body']);
-                    $this->assertStringContainsString('Service2 limit is 10.', $response['body']);
-                    $this->assertStringContainsString('Always reset works.', $response['body']);
-                    $this->assertStringContainsString('Safe always reseter is not a proxy.', $response['body']);
-                    $this->assertStringContainsString('Safe Always reset works.', $response['body']);
-                    $this->assertStringContainsString('TmpRepo was proxified.', $response['body']);
-                    $this->assertStringContainsString('TmpRepo limit is 15.', $response['body']);
-                    $this->assertStringContainsString('Connection limit is 12.', $response['body']);
-
-                    $wg->done();
-                });
-            }
-
-            $wg->wait(10);
-            $end = microtime(true);
-
-            $client = HttpClient::fromDomain('localhost', 9999, false);
-            $this->assertTrue($client->connect());
-            $response = $client->send('/sleep')['response']; // request sleeps for 2 seconds
-            $this->assertSame(200, $response['statusCode']);
-            $this->assertStringContainsString('text/html', $response['headers']['content-type']);
-            $this->assertStringContainsString('Check was true.', $response['body']);
-            $this->assertStringContainsString('Checks: 10.', $response['body']);
-
-            // without coroutines, it should be 20, expected is 2, 3s is slowness tolerance in initialization
-            // with coverage enabled
-            self::assertLessThan(5, $end - $start);
-        });
-    }
-
-    public function testCoroutinesWithDoctrineAndWithDebugOff(): void
-    {
-        $clearCache = $this->createConsoleProcess([
-            'cache:clear',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
-        $clearCache->setTimeout(5);
-        $clearCache->disableOutput();
-        $clearCache->run();
-
-        $this->assertProcessSucceeded($clearCache);
-
-        $dropSchema = $this->createConsoleProcess([
-            'doctrine:schema:drop',
-            '--full-database',
-            '--force',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        $dropSchema = $this->createConsoleProcess(
+            [
+                'doctrine:schema:drop',
+                '--full-database',
+                '--force',
+            ],
+            $envs
+        );
         $dropSchema->setTimeout(5);
         $dropSchema->disableOutput();
         $dropSchema->run();
 
         $this->assertProcessSucceeded($dropSchema);
 
-        $migrations = $this->createConsoleProcess([
-            'doctrine:migrations:migrate',
-            '--no-interaction',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        $migrations = $this->createConsoleProcess(
+            [
+                'doctrine:migrations:migrate',
+                '--no-interaction',
+            ],
+            $envs
+        );
         $migrations->setTimeout(5);
         $migrations->disableOutput();
         $migrations->run();
 
         $this->assertProcessSucceeded($migrations);
 
-        $serverStart = $this->createConsoleProcess([
-            'swoole:server:start',
-            '--host=localhost',
-            '--port=9999',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        $serverStart = $this->createConsoleProcess(
+            [
+                'swoole:server:start',
+                '--host=localhost',
+                '--port=9999',
+            ],
+            $envs
+        );
 
         $serverStart->setTimeout(5);
         $serverStart->disableOutput();
@@ -253,120 +190,26 @@ final class SwooleServerCoroutinesTest extends ServerTestCase
         });
     }
 
-    public function testCoroutinesWithDoctrineAndWithDebugOn(): void
+    /**
+     * @dataProvider coroutineTestDataProviderForTaskWorkers
+     */
+    public function testCoroutinesAndTaskWorkersWithEnvs(array $envs): void
     {
-        $clearCache = $this->createConsoleProcess([
-            'cache:clear',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
+        $clearCache = $this->createConsoleProcess(['cache:clear'], $envs);
         $clearCache->setTimeout(5);
         $clearCache->disableOutput();
         $clearCache->run();
 
         $this->assertProcessSucceeded($clearCache);
 
-        $dropSchema = $this->createConsoleProcess([
-            'doctrine:schema:drop',
-            '--full-database',
-            '--force',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
-        $dropSchema->setTimeout(5);
-        $dropSchema->disableOutput();
-        $dropSchema->run();
-
-        $this->assertProcessSucceeded($dropSchema);
-
-        $migrations = $this->createConsoleProcess([
-            'doctrine:migrations:migrate',
-            '--no-interaction',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
-        $migrations->setTimeout(5);
-        $migrations->disableOutput();
-        $migrations->run();
-
-        $this->assertProcessSucceeded($migrations);
-
-        $serverStart = $this->createConsoleProcess([
-            'swoole:server:start',
-            '--host=localhost',
-            '--port=9999',
-        ], ['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']);
-
-        $serverStart->setTimeout(5);
-        $serverStart->disableOutput();
-        $serverStart->run();
-
-        $this->assertProcessSucceeded($serverStart);
-
-        $this->runAsCoroutineAndWait(function (): void {
-            $this->deferServerStop();
-
-            $initClient = HttpClient::fromDomain('localhost', 9999, false);
-            $this->assertTrue($initClient->connect());
-
-            $start = microtime(true);
-            $wg = new WaitGroup();
-
-            for ($i = 0; $i < 20; ++$i) {
-                go(function () use ($wg): void {
-                    $wg->add();
-                    $client = HttpClient::fromDomain('localhost', 9999, false);
-                    $this->assertTrue($client->connect());
-                    $response = $client->send('/doctrine')['response'];
-                    $this->assertSame(200, $response['statusCode']);
-                    $this->assertStringContainsString('text/html', $response['headers']['content-type']);
-                    $wg->done();
-                });
-            }
-
-            $wg->wait(10);
-            $end = microtime(true);
-
-            self::assertLessThan(self::coverageEnabled() ? 5 : 0.5, $end - $start);
-
-            $client = HttpClient::fromDomain('localhost', 9999, false);
-            $this->assertTrue($client->connect());
-            $response = $client->send('/doctrine-resets')['response']; // request sleeps for 2 seconds
-            $this->assertSame(200, $response['statusCode']);
-            $this->assertStringContainsString('application/json', $response['headers']['content-type']);
-
-            $resets = $response['body'];
-            self::assertCount(2, $resets);
-
-            foreach ($resets as $reset) {
-                // it cannot be determined how many connections are created
-                self::assertGreaterThan(0, $reset);
-            }
-        });
-    }
-
-    public function testCoroutinesWithTaskWorkers(): void
-    {
-        $clearCache = $this->createConsoleProcess([
-            'cache:clear',
-        ], [
-            'APP_ENV' => 'coroutines',
-            'APP_DEBUG' => '0',
-            'WORKER_COUNT' => '1',
-            'TASK_WORKER_COUNT' => '1',
-            'REACTOR_COUNT' => '1',
-        ]);
-        $clearCache->setTimeout(5);
-        $clearCache->disableOutput();
-        $clearCache->run();
-
-        $this->assertProcessSucceeded($clearCache);
-
-        $serverStart = $this->createConsoleProcess([
-            'swoole:server:start',
-            '--host=localhost',
-            '--port=9999',
-        ], [
-            'APP_ENV' => 'coroutines',
-            'APP_DEBUG' => '0',
-            'WORKER_COUNT' => '1',
-            'TASK_WORKER_COUNT' => '1',
-            'REACTOR_COUNT' => '1',
-        ]);
+        $serverStart = $this->createConsoleProcess(
+            [
+                'swoole:server:start',
+                '--host=localhost',
+                '--port=9999',
+            ],
+            $envs
+        );
 
         $serverStart->setTimeout(5);
         $serverStart->disableOutput();
@@ -418,67 +261,53 @@ final class SwooleServerCoroutinesTest extends ServerTestCase
         self::assertCount(3, $lines);
     }
 
-    public function testCoroutinesWithTaskWorkersWithDoctrine(): void
+    /**
+     * @dataProvider coroutineTestDataProviderForTaskWorkers
+     */
+    public function testCoroutinesWithTaskWorkersWithDoctrine(array $envs): void
     {
-        $clearCache = $this->createConsoleProcess([
-            'cache:clear',
-        ], [
-            'APP_ENV' => 'coroutines',
-            'APP_DEBUG' => '0',
-            'WORKER_COUNT' => '1',
-            'TASK_WORKER_COUNT' => '1',
-            'REACTOR_COUNT' => '1',
-        ]);
+        $clearCache = $this->createConsoleProcess(['cache:clear'], $envs);
         $clearCache->setTimeout(5);
         $clearCache->disableOutput();
         $clearCache->run();
 
         $this->assertProcessSucceeded($clearCache);
 
-        $dropSchema = $this->createConsoleProcess([
-            'doctrine:schema:drop',
-            '--full-database',
-            '--force',
-        ], [
-            'APP_ENV' => 'coroutines',
-            'APP_DEBUG' => '0',
-            'WORKER_COUNT' => '1',
-            'REACTOR_COUNT' => '1',
-            'TASK_WORKER_COUNT' => '1',
-        ]);
+        $dropSchema = $this->createConsoleProcess(
+            [
+                'doctrine:schema:drop',
+                '--full-database',
+                '--force',
+            ],
+            $envs
+        );
         $dropSchema->setTimeout(5);
         $dropSchema->disableOutput();
         $dropSchema->run();
 
         $this->assertProcessSucceeded($dropSchema);
 
-        $migrations = $this->createConsoleProcess([
-            'doctrine:migrations:migrate',
-            '--no-interaction',
-        ], [
-            'APP_ENV' => 'coroutines',
-            'APP_DEBUG' => '0',
-            'WORKER_COUNT' => '1',
-            'REACTOR_COUNT' => '1',
-            'TASK_WORKER_COUNT' => '1',
-        ]);
+        $migrations = $this->createConsoleProcess(
+            [
+                'doctrine:migrations:migrate',
+                '--no-interaction',
+            ],
+            $envs
+        );
         $migrations->setTimeout(5);
         $migrations->disableOutput();
         $migrations->run();
 
         $this->assertProcessSucceeded($migrations);
 
-        $serverStart = $this->createConsoleProcess([
-            'swoole:server:start',
-            '--host=localhost',
-            '--port=9999',
-        ], [
-            'APP_ENV' => 'coroutines',
-            'APP_DEBUG' => '0',
-            'WORKER_COUNT' => '1',
-            'TASK_WORKER_COUNT' => '1',
-            'REACTOR_COUNT' => '1',
-        ]);
+        $serverStart = $this->createConsoleProcess(
+            [
+                'swoole:server:start',
+                '--host=localhost',
+                '--port=9999',
+            ],
+            $envs
+        );
 
         $serverStart->setTimeout(5);
         $serverStart->disableOutput();
@@ -522,6 +351,84 @@ final class SwooleServerCoroutinesTest extends ServerTestCase
         $count = $repo->count([]);
 
         self::assertSame(3, $count);
+    }
+
+    public function coroutineTestDataProvider(): array
+    {
+        return [
+            // debug on
+            [['APP_ENV' => 'coroutines', 'APP_DEBUG' => '1', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']],
+            // debug off
+            [['APP_ENV' => 'coroutines', 'APP_DEBUG' => '0', 'WORKER_COUNT' => '1', 'REACTOR_COUNT' => '1']],
+            // prod env with inline container factories and debug on
+            [
+                [
+                    'APP_ENV' => 'prod',
+                    'APP_DEBUG' => '1',
+                    'OVERRIDE_PROD_ENV' => 'coroutines',
+                    'WORKER_COUNT' => '1',
+                    'REACTOR_COUNT' => '1',
+                ],
+            ],
+            // prod env with inline container factories and debug off
+            [
+                [
+                    'APP_ENV' => 'prod',
+                    'APP_DEBUG' => '0',
+                    'OVERRIDE_PROD_ENV' => 'coroutines',
+                    'WORKER_COUNT' => '1',
+                    'REACTOR_COUNT' => '1',
+                ],
+            ],
+        ];
+    }
+
+    public function coroutineTestDataProviderForTaskWorkers(): array
+    {
+        return [
+            // debug on
+            [
+                [
+                    'APP_ENV' => 'coroutines',
+                    'APP_DEBUG' => '1',
+                    'WORKER_COUNT' => '1',
+                    'TASK_WORKER_COUNT' => '1',
+                    'REACTOR_COUNT' => '1',
+                ],
+            ],
+            // debug off
+            [
+                [
+                    'APP_ENV' => 'coroutines',
+                    'APP_DEBUG' => '0',
+                    'WORKER_COUNT' => '1',
+                    'TASK_WORKER_COUNT' => '1',
+                    'REACTOR_COUNT' => '1',
+                ],
+            ],
+            // prod env with inline container factories and debug on
+            [
+                [
+                    'APP_ENV' => 'prod',
+                    'APP_DEBUG' => '1',
+                    'OVERRIDE_PROD_ENV' => 'coroutines',
+                    'WORKER_COUNT' => '1',
+                    'TASK_WORKER_COUNT' => '1',
+                    'REACTOR_COUNT' => '1',
+                ],
+            ],
+            // prod env with inline container factories and debug off
+            [
+                [
+                    'APP_ENV' => 'prod',
+                    'APP_DEBUG' => '0',
+                    'OVERRIDE_PROD_ENV' => 'coroutines',
+                    'WORKER_COUNT' => '1',
+                    'TASK_WORKER_COUNT' => '1',
+                    'REACTOR_COUNT' => '1',
+                ],
+            ],
+        ];
     }
 
     private function generateNotExistingCustomTestFile(): string
