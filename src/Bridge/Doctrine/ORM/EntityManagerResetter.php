021bd1f82e300ef40394d5c6a673efacc05990f5
Rastusik <mfris@pixelfederation.com>
2023-02-03 15:10:35 +0100

    fix(coroutines): added entity manager resetter, which is decoupled from the stability checker, so it can be overridden easily

---
+++ src/Bridge/Doctrine/ORM/EntityManagerResetter.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace K911\Swoole\Bridge\Doctrine\ORM;
+
+use Doctrine\Persistence\ObjectManager;
+use K911\Swoole\Bridge\Symfony\Container\Resetter;
+
+final class EntityManagerResetter implements Resetter
+{
+    public function reset(object $service): void
+    {
+        if (!$service instanceof ObjectManager) {
+            throw new \UnexpectedValueException(\sprintf('Invalid service - expected %s, got %s', ObjectManager::class, \get_class($service)));
+        }
+
+        $service->clear();
+    }
+}
