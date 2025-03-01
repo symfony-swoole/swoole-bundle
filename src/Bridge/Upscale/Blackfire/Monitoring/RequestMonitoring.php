d7c3a63f9e6467adc2f799de49e24a737b9231e9
Rastusik <1735097+Rastusik@users.noreply.github.com>
2025-03-01 17:16:59 +0100

    fix(blackfire): minor fixes to balckfire monitoring feature

--- src/Bridge/Upscale/Blackfire/Monitoring/RequestMonitoring.php
+++ src/Bridge/Upscale/Blackfire/Monitoring/RequestMonitoring.php
@@ -9,11 +9,17 @@ use Closure;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use SwooleBundle\SwooleBundle\Bridge\Symfony\HttpFoundation\RequestFactory;
+use SwooleBundle\SwooleBundle\Common\System\System;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
-final readonly class RequestMonitoring
+final class RequestMonitoring
 {
-    public function __construct(private RequestFactory $requestFactory) {}
+    private string|null $blackfireVersion = null;
+
+    public function __construct(
+        private RequestFactory $requestFactory,
+        private System $system,
+    ) {}
 
     public function monitor(Closure $fn, Request $request, Response $response): void
     {
@@ -29,11 +35,48 @@ final readonly class RequestMonitoring
 
     private function start(SymfonyRequest $request): void
     {
-        BlackfireProbe::startTransaction();
+        $blackfireVersion = $this->getBlackfireVersion();
+
+        if ($blackfireVersion === '') {
+            return;
+        }
+
+        $transactionName = $request->getMethod() . ' ' . $request->getPathInfo();
+
+        if (version_compare($blackfireVersion, '1.78.0', '>=')) {
+            BlackfireProbe::startTransaction($transactionName);
+        } else {
+            BlackfireProbe::startTransaction();
+            BlackfireProbe::setTransactionName($transactionName);
+        }
+
+        if (!method_exists(BlackfireProbe::class, 'setAttribute')) {
+            return;
+        }
+
+        BlackfireProbe::setAttribute('http.target', $request->getPathInfo());
+        BlackfireProbe::setAttribute('http.url', $request->getRequestUri());
+        BlackfireProbe::setAttribute('http.method', $request->getMethod());
+        BlackfireProbe::setAttribute('http.host', $request->getHost());
+        BlackfireProbe::setAttribute('host', $request->getHost());
+        BlackfireProbe::setAttribute('framework', sprintf('Symfony with %s', $this->system->extension()->toString()));
     }
 
     private function stop(): void
     {
+        if ($this->getBlackfireVersion() === '') {
+            return;
+        }
+
         BlackfireProbe::stopTransaction();
+    }
+
+    private function getBlackfireVersion(): string
+    {
+        if ($this->blackfireVersion === null) {
+            $this->blackfireVersion = (($v = phpversion('blackfire')) === false ? '' : $v);
+        }
+
+        return $this->blackfireVersion;
     }
 }
