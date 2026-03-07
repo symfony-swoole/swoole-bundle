d0bdf5eb4e2a3c9be85a14b0e1191536ec424a9c
Martin Fris <1735097+Rastusik@users.noreply.github.com>
2026-03-07 20:07:31 +0100

    Refactor resetter handling in StatefulServicesPass

--- src/Bridge/Symfony/Bundle/DependencyInjection/CompilerPass/StatefulServicesPass.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/CompilerPass/StatefulServicesPass.php
@@ -172,15 +172,16 @@ final class StatefulServicesPass implements CompilerPassInterface
             if ($resetter !== null && str_starts_with($resetter, '?')) {
                 $definition = $container->findDefinition($serviceId);
                 $definitionClass = $definition->getClass();
+                    
                 if ($definitionClass !== null && interface_exists($definitionClass)) {
                     $resetter = null;
                 } else {
-                Assertion::classExists($definitionClass);
-                $resetter = substr($resetter, 1);
+                    Assertion::classExists($definitionClass);
+                    $resetter = substr($resetter, 1);
 
-                if (!method_exists($definitionClass, $resetter)) {
-                    $resetter = null;
-                }
+                    if (!method_exists($definitionClass, $resetter)) {
+                        $resetter = null;
+                    }
                 }
             }
 
