d6daf5de0cdb19eaa1c118c935958671c78bd5aa
Rastusik <1735097+Rastusik@users.noreply.github.com>
2026-07-26 21:55:16 +0200

    fix(coroutines): added unserialization logic to proxies to enable data collectors to work

---
+++ src/Bridge/Symfony/Container/ServicePool/StaticServicePool.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwooleBundle\SwooleBundle\Bridge\Symfony\Container\ServicePool;
+
+/**
+ * @template T of object
+ * @template-implements ServicePool<T>
+ */
+final readonly class StaticServicePool implements ServicePool
+{
+    /**
+     * @param T $instance
+     */
+    public function __construct(
+        private object $instance,
+    ) {}
+
+    /**
+     * @return T
+     */
+    public function get(): object
+    {
+        return $this->instance;
+    }
+
+    /**
+     * @return T
+     */
+    public function getAssigned(): object
+    {
+        return $this->instance;
+    }
+
+    public function releaseFromCoroutine(): void
+    {
+        // no need to release anything
+    }
+}
